3ba8cac8748f5e7e32b0b679756cf69cfce4461b
Lun-Dev <lunthedev@gmail.com>
2021-11-11 13:42:01 +0800

    Hello

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ function populateStorage() {
     localStorage.setItem('bgcolor', 'red');
     localStorage.setItem('font', 'Helvetica');
     localStorage.setItem('image', 'myCat.png');
+    console.log("hi")
   }
 
   populateStorage()
